414e45f6e011808700a7b3cd1a54c3c1296ea294
Faisal Inayath <faisalinayath16@gmail.com>
2025-01-31 16:17:07 +0530

    Remove Comment from app module

--- hyphothesis/app/api/__init__.py
+++ hyphothesis/app/api/__init__.py
@@ -2,7 +2,6 @@ from flask import Blueprint
 
 api_blueprint = Blueprint('api', __name__)
 
-# Import and register individual blueprints
 from .z_test_api import ztest_api
 
 api_blueprint.register_blueprint(ztest_api)
