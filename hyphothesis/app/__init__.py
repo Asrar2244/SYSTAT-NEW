a4cc2bbd8ce39a5880b4b61afadb754df7f15016
Faisal Inayath <faisalinayath16@gmail.com>
2025-01-31 16:19:27 +0530

    Remove comment

--- hyphothesis/app/__init__.py
+++ hyphothesis/app/__init__.py
@@ -4,7 +4,6 @@ from app.api import api_blueprint
 def create_app():
     app = Flask(__name__)
 
-    # Register the API blueprint
     app.register_blueprint(api_blueprint, url_prefix='/hyphothesis/api')
 
     return app
